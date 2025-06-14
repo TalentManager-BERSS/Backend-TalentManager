32601a4884c01a3f0669554c31ae5739227ba5fb
Gabriel Borja <borjagabriel45@gmail.com>
2025-06-13 20:09:14 -0500

    first commit

---
+++ src/main/java/com/berss/platform/TalentmanagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.berss.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentmanagerBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalentmanagerBackendApplication.class, args);
+    }
+
+}
