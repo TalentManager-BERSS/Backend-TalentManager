81723d0fc79b6156db8e177a339025d87a3898c8
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-07-08 06:42:42 -0500

    Feat: Update CorsConfig.java

--- src/main/java/com/berss/platform/config/CorsConfig.java
+++ src/main/java/com/berss/platform/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://talentmanagerapplication.netlify.app")
+                        .allowedOrigins("https://talentmanagerapp.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
