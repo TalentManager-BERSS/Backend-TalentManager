0327be491372174ec6594f7d6a7b9dcadbd8be19
SantiagoCárdenas <166081997+Sack136@users.noreply.github.com>
2025-07-08 06:29:41 -0500

    Feat: Update CorsConfig.java

--- src/main/java/com/berss/platform/config/CorsConfig.java
+++ src/main/java/com/berss/platform/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://sweet-cascaron-5606c7.netlify.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
